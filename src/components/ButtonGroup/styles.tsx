ace6a1cdac8d70a1f8207e0dd9cf2186460d3284
Lucas Nobre <lucasnobre37@outlook.com>
2023-05-12 18:54:50 -0300

    create button group component

---
+++ src/components/ButtonGroup/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  gap: 20px;
+  width: 100%;
+  margin: 100px 0 0 0;
+`;
